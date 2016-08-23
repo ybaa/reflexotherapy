94d2abbde9f45c4c697676dffaf06c1c2f66b689
Martyna Łagożna <m.lagozna@live.com>
2016-08-23 14:21:09 +0200

    little change with repeat slides

--- slider.js
+++ slider.js
@@ -25,11 +25,11 @@ function changeSlide(){
 
 
 $(document).ready(function(){
-	var count = 0;
+	var count = 1;
 	var images = ["images/slide1.png","images/slide2.png","images/slide3.png","images/slide4.png","images/slide5.png"];
 	var image = $(".sliderek");
 
-	image.css("background-image","url("+images[count++]+")");
+	//image.css("background-image","url("+images[count]+")");
 
 	setInterval(function(){
 		image.fadeOut(500, function(){
