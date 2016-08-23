bbe19360641c0604363432ff32af74b60ac691b6
Martyna Łagożna <m.lagozna@live.com>
2016-08-23 13:59:46 +0200

    fighting agains slider! It behaves like a slider but I don't want to blink with everything else at the same time

---
+++ slider.js
@@ -0,0 +1,44 @@
+/*var numberOfSlide = Math.floor(Math.random()*5)+1;
+
+
+	function hide(){
+		$("#sliderek").fadeOut(500);
+	}
+
+
+function changeSlide(){
+	numberOfSlide++;
+	if (numberOfSlide > 5)
+		numberOfSlide = 1;
+	var file = "<img src = \"simages/"+"slide"+numberOfSlide + ".png\"/>";
+	
+	document.getElementById("sliderek").innerHTML = file;
+	
+	$("#sliderek").fadeIn(500);
+
+	setTimeout("changeSlide()",5000);
+	setTimeout("hide()",4500);
+}
+
+*/
+
+
+
+$(document).ready(function(){
+	var count = 0;
+	var images = ["images/slide1.png","images/slide2.png","images/slide3.png","images/slide4.png","images/slide5.png"];
+	var image = $(".sliderek");
+
+	image.css("background-image","url("+images[count++]+")");
+
+	setInterval(function(){
+		image.fadeOut(500, function(){
+			image.css("background-image","url("+images[count++]+")");
+			image.fadeIn(500);
+		});
+
+		if(count == images.length){
+			count = 0;
+		}
+	},5000);
+});
